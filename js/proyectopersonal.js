aa3c0c3be585b131b86bbd2f8a2267383fa401a7
AgusPressel <40279471+AgusPressel@users.noreply.github.com>
2018-07-22 23:04:46 -0300

    0.4.5
    aumento de la trasiscion de js para las habilidades  blandas

--- js/proyectopersonal.js
+++ js/proyectopersonal.js
@@ -11,44 +11,44 @@ $("#relacion").hide();
 
     $("#trabajo1").mouseenter(function(){
         $("#trabajo").show();
-        $("#trabajo").css("transition", "0.3s");
+        $("#trabajo").css("transition", "0.7s");
     });
     $("#proactividad2").mouseenter(function(){
         $("#proactividad").show();
-        $("#proactividad").css("transition", "0.3s");
+        $("#proactividad").css("transition", "0.7s");
     });
     $("#capacidad2").mouseenter(function(){
          $("#capacidad").show();
-         $("#capacidad").css("transition", "0.3s");
+         $("#capacidad").css("transition", "0.7s");
     });
     $("#responsabilidad1").mouseenter(function(){
         $("#responsabilidad").show();
-        $("#responsabilidad").css("transition", "0.3s");
+        $("#responsabilidad").css("transition", "0.7s");
     });
 
     $("#relacion1").mouseenter(function(){
         $("#relacion").show();
-        $("#relacion").css("transition", "0.3s");
+        $("#relacion").css("transition", "0.7s");
     });
 
   $("#trabajo1").mouseleave(function(){
         $("#trabajo").hide();
-        $("#trabajo").css("transition", "0.3s");
+        $("#trabajo").css("transition", "0.7s");
     });
     $("#proactividad2").mouseleave(function(){
         $("#proactividad").hide();
-        $("#proactividad").css("transition", "0.3s");
+        $("#proactividad").css("transition", "0.7s");
     });
     $("#capacidad2").mouseleave(function(){
          $("#capacidad").hide();
-         $("#capacidad").css("transition", "0.3s");
+         $("#capacidad").css("transition", "0.7s");
     });
     $("#responsabilidad1").mouseleave(function(){
         $("#responsabilidad").hide();
-        $("#responsabilidad").css("transition", "0.3s");
+        $("#responsabilidad").css("transition", "0.7s");
     });
     $("#relacion1").mouseleave(function(){
         $("#relacion").hide();
-        $("#relacion").css("transition", "0.3s");
+        $("#relacion").css("transition", "0.7s");
     });
 });
