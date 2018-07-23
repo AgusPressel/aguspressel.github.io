d8013ebdb2a7f9ae972ec017cd8d08c3929bbb55
AgusPressel <40279471+AgusPressel@users.noreply.github.com>
2018-07-22 21:01:46 -0300

    version 0.4
    actualizacione de todos los datos y paginas
    
    cambio de formatos

---
+++ js/proyectopersonal.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+	//esta  funcion permite indicar en que pagina se encuentra
+   
+/*
+   $("#hideAm").hide();
+	$("#hideEx").hide();
+	$("#hideCon").hide();
+	$("#horizontes").hide();
+	$("#Expe").hide();
+	$("#Convertite").hide();
+
+   	// este boton esconde el texto amplia tus orizontes
+    $("#hideAm").mouseleave(function(){
+        $("#horizontes").hide();
+        $("#hideAm").hide();
+        $("#showAm").show();
+    });
+    // este boton muestra el texto amplia tus orizontes
+    $("#showAm").mouseenter(function(){
+        $("#horizontes").show();
+        $("#hideAm").show();
+        $("#showAm").hide();
+    });
+    //-----------------------------------------------
+	 // este boton muestra el texto amplia tus orizontes
+    $("#showEx").mouseenter(function(){
+        $("#Expe").show();
+        $("#hideEx").show();
+        $("#showEx").hide();
+    });
+	// este boton esconde el texto amplia tus orizontes
+	$("#hideEx").mouseleave(function(){
+        $("#Expe").hide();
+        $("#hideEx").hide();
+        $("#showEx").show();
+
+    });
+
+    //----------------------------------------------
+	// este boton esconde el texto amplia tus orizontes
+	$("#hideCon").mouseleave(function(){
+        $("#Convertite").hide();
+        $("#hideCon").hide();
+        $("#showCon").show();
+    });
+    // este boton muestra el texto amplia tus orizontes
+    $("#showCon").mouseenter(function(){
+        $("#Convertite").show();
+        $("#hideCon").show();
+        $("#showCon").hide();
+    });
+
+*/
+
+
+$("#trabajo").hide();
+$("#proactividad").hide();
+$("#capacidad").hide();
+$("#responsabilidad").hide();
+$("#relacion").hide();
+/*
+  $("#trabajo1").click(function(){
+    $("trabajo").hide();
+    });*/
+
+/*
+  $("#trabajo1").mouseenter(function(){
+    $("trabajo").show();
+    });
+$("#trabajo1").mouseleave(function(){
+        
+$("#trabajo").hide();
+    });
+*/
+});
